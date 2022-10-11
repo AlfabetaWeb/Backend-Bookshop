60b32cec55ee4828eec1dc64b8d8fae8e1c940b4
B. A. Ballesteros <104979664+AlfabetaWeb@users.noreply.github.com>
2022-10-11 18:44:00 +0200

    feat: Create user functions

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
@@ -35,5 +36,52 @@ class AuthController extends Controller
         return response()->json(compact('user', 'token'), 201);
     }
 
+    //THIS FUNCTION ALLOWS LOGIN REGISTERED USER
+    public function login(Request $request)
+        {
+            $input = $request->only('email', 'password');
+            $jwt_token = null;
+
+            if (!$jwt_token = JWTAuth::attempt($input)) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Invalid Email or Password',
+                ], Response::HTTP_UNAUTHORIZED);
+            }
+
+            return response()->json([
+                'success' => true,
+                'token' => $jwt_token,
+            ]);
+        }
+
+    //THIS FYNCTION GETS USER    
+        public function me()
+        {
+            return response()->json(auth()->user());;
+        }
+    
+    //THIS FUNCTION ALLOWS USER TO LOG OUT
+        public function logout(Request $request)
+        {
+            $this->validate($request, [
+                'token' => 'required'
+            ]);
+    
+            try {
+                JWTAuth::invalidate($request->token);
+    
+                return response()->json([
+                    'success' => true,
+                    'message' => 'User logged out successfully'
+                ]);
+            } catch (\Exception $exception) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Sorry, the user cannot be logged out'
+                ], Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+        }
+    
 
 }
